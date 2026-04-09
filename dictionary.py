f8a97570e03092ac38157d0424822636e53d7d32
Fern-Pyro <lorellana3@cps.edu>
2026-04-09 11:25:48 -0500

    dictionary_update

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,9 @@
 import wikipedia
-import re 
+import re, time
 from typing import List
 
 
-def tokenize(self, text: str) -> List[str]:
+def tokenize(text: str) -> List[str]:
         """Splits given text into a list of the individual tokens in order
 
         Args:
@@ -26,23 +26,47 @@ def tokenize(self, text: str) -> List[str]:
                 if token != "":
                     tokens.append(token.lower())
                     token = ""
-                if c.strip() != "":
-                    tokens.append(str(c.strip()))
+                # if c.strip() != "":
+                #     tokens.append(str(c.strip()))
 
         if token != "":
             tokens.append(token.lower())
         return tokens
 
+
 article = wikipedia.page("Artemis II", auto_suggest=False).content
-tokens = article.tokenize
-print(article)
+#print(article)
+words = tokenize(article)
+#print(words)
 
-freqs = []
+with open("sorted_stoplist.txt", "r", encoding="utf8") as f:
+     stoplist = f.read()
+tokenized_stoplist = tokenize(stoplist)
 
-for word in tokens:
-     if word is freqs:
-          freqs[word] += 1
-     else:
-          freqs[word] = 1
 
-print(freqs)
+# with open("sorted_stoplist.txt", "r", encoding='utf8') as f:
+#      stoplist = f.read()
+
+freqs = {}
+
+for word in words:
+    if word not in tokenized_stoplist: 
+        if word is freqs:
+            freqs[word] += 1
+        else:
+            freqs[word] = 1
+
+#print(freqs)
+
+#Print total unique words and total number of words
+unique_words = len(freqs)
+print(f"Unique words: {unique_words}")
+
+total_num_words = sum(freqs.values())
+print(f"Total words: {total_num_words}")
+
+#Print top 20 words
+# top_words = sorted(freqs.items(), key=lambda x: x[1], reverse = True)
+# print("Top 20 words: ")
+# for word, count in top_words[:20]:
+#     print(f"{word} {count}")
